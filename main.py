86088e2b09c4f32326687b61564dcb095a1ccb87
ffserro <ffserro@hotmail.com>
2024-04-10 17:01:38 -0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.write('Exercício de comunicação do Tubarão Makão')
+st.write('EXERCÍCIO DE COMUNICAÇÃO DO TUBARÃO MAKÃO')
 
 texto = st.text_area(label='Digite aqui a mensagem a ser convertida:', value="")
